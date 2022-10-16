1b8642f950bb8232946bf0b7ff6f7ccca67f848d
VishwanGithub <vishwan2000av@gmail.com>
2022-10-16 14:34:47 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express=require("express");
+var path=require("path");
+var app=express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get("/",(req,res)=>{
+
+    res.sendFile(__dirname+"/public/home.html");
+
+});
+
+
+app.listen(3000,()=>{
+    console.log("listening at 3000");
+});
